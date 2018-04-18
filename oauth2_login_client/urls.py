bd5f52ac7da859651547ae2b0fa26dfe8a4a58ee
Richard Mansfield <richard@dragonfly.co.nz>
2018-04-19 11:12:41 +1200

    Name urls

--- oauth2_login_client/urls.py
+++ oauth2_login_client/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from .views import login_redirect, account_redirect, login_callback
 
 urlpatterns = [
-    url(r'^login/callback', login_callback),
-    url(r'^login', login_redirect),
-    url(r'^account', account_redirect),
+    url(r'^login/callback', login_callback, name='oauth2_client_login_callback'),
+    url(r'^login', login_redirect, name='oauth2_client_login_redirect'),
+    url(r'^account', account_redirect, name='oauth2_client_account_redirect'),
 ]
