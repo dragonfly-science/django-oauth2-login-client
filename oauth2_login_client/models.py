45006b8dd071fbbb5ab533831b00ba317159885c
Richard Mansfield <richard@dragonfly.co.nz>
2019-05-06 16:18:59 +1200

    Update max length of remote username

--- oauth2_login_client/models.py
+++ oauth2_login_client/models.py
@@ -5,4 +5,4 @@ class RemoteUser(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, help_text="Django user",
         primary_key=True, on_delete=models.CASCADE)
-    remote_username = models.CharField(max_length=30, unique=True)
+    remote_username = models.CharField(max_length=150, unique=True)
