6c944a650102cc27aa12b971b77d0e9122b0989f
Richard Mansfield <richard@dragonfly.co.nz>
2023-06-14 14:22:05 +1200

    Move to re_path

--- oauth2_login_client/urls.py
+++ oauth2_login_client/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url, include
+from django.conf.urls import include
+from django.urls import re_path
 from .views import login_redirect, account_redirect, login_callback
 
 urlpatterns = [
-    url(r'^login/callback', login_callback, name='oauth2_client_login_callback'),
-    url(r'^login', login_redirect, name='oauth2_client_login_redirect'),
-    url(r'^account', account_redirect, name='oauth2_client_account_redirect'),
+    re_path(r'^login/callback', login_callback, name='oauth2_client_login_callback'),
+    re_path(r'^login', login_redirect, name='oauth2_client_login_redirect'),
+    re_path(r'^account', account_redirect, name='oauth2_client_account_redirect'),
 ]
